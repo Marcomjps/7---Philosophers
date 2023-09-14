290cb460a10ce9e010de96eaaae5df6a7f1f7967
Marco Silva <marsilva@c1r16s1.42porto.com>
2023-09-14 18:25:21 +0100

    egfeg

---
+++ 1_Philosophers/src/a_philosophers.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   a_philosophers.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsilva <marsilva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/10 10:12:03 by sgoffaux          #+#    #+#             */
+/*   Updated: 2023/08/25 10:32:36 by marsilva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int main(int argc, char **argv)
+{
+	t_data data;
+
+	if(b_struck_args( &data.args ,argc ,argv) == 0)
+		return(0);
+	
+	return(1);
+}
